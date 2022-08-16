e6d55ee204bb932c8b8b19f40881f5cb4a8a770c
Andjela24 <1andjela.ljubenkovic@gmail.com>
2022-08-17 01:12:16 +0200

    IT255-DZ14

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ConsoleSpy } from './utils';
+
+var fakeConsole: ConsoleSpy;
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+    fakeConsole = new ConsoleSpy();
+    let originalConsole = window.console;
+    (<any>window).console = fakeConsole;
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +27,29 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MetHotels'`, () => {
+  it(`should have as title 'met-hotels'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MetHotels');
+    expect(app.title).toEqual('met-hotels');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MetHotels app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('met-hotels app is running!');
   });
+
+
+
+  it('handles form changes', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    let input = fixture.debugElement.query(By.css('input')).nativeElement;
+    input.value = 'XYZ';
+    dispatchEvent(input);
+    tick();
+
+    expect(fakeConsole.logs).toContain('form changed to: [object Object]');
+  }));
 });
